c16ae3dfc577130a46b095c033ee98d4d8c043c7
V-5000-dev <victor101001000@gmail.com>
2026-05-31 11:59:38 -0400

    changes

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    public Transform target;
+    public float range = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
